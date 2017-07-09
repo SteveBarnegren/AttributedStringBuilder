71238358febf965195d9047f3784702708448815
Steve Barnegren <steve.barnegren@gmail.com>
2017-07-09 08:42:52 +0100

    Project / framework setup

---
+++ AttributedStringBuilder/AttributedStringBuilder/AttributedStringBuilder.swift
@@ -0,0 +1,19 @@
+//
+//  AttributedStringBuilder.swift
+//  AttributedStringBuilder
+//
+//  Created by Steve Barnegren on 09/07/2017.
+//  Copyright © 2017 Steve Barnegren. All rights reserved.
+//
+
+import Foundation
+
+public class AttributedStringBuilder {
+    
+    public init() {}
+    
+    
+    
+    
+    
+}
---
+++ Example/Example.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import AttributedStringBuilder
+
+var str = "Hello, playground"
+
+let builder = AttributedStringBuilder()
