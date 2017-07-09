df1005742cc0edf34bb0911f08b255dba99736b7
Steve Barnegren <steve.barnegren@gmail.com>
2017-07-09 16:58:44 +0100

    Added some default values

--- AttributedStringBuilder/AttributedStringBuilder/AttributedStringBuilder.swift
+++ AttributedStringBuilder/AttributedStringBuilder/AttributedStringBuilder.swift
@@ -10,7 +10,112 @@ import Foundation
 
 public class AttributedStringBuilder {
     
+
+
+    private var masterAttributedString = NSMutableAttributedString()
+    
+    public var attributedString: NSAttributedString {
+        return masterAttributedString
+    }
+    
+    // MARK: - Defaults
+    
+    public var defaultFont = UIFont.systemFont(ofSize: 12, weight: UIFontWeightMedium)
+    public var defaultTextColor = UIColor.darkText
+    public var defaultBackgroundColor: UIColor? = nil
+    public var defaultKerning = 1.0
+    public var defaultUseStrikethrough = false
+    public var defaultUseUnderline = false
+    public var defaultStrokeColor: UIColor? = nil
+    public var defaultStokeWidth = 0.0
+    
+    
+    // MARK: - Methods
+    
     public init() {}
+    
+    public func appendText(_ text: String) {
+        
+        let attributes = makeAttributes()
+        let string = NSAttributedString(string: text, attributes: attributes)
+        masterAttributedString.append(string)
+    }
+    
+    // MARK: - Create attributes
+    
+    private func makeAttributes(withOverrides overrides: Dictionary<String, Any>) -> Dictionary<String, Any> {
+        
+        var attributes = makeAttributes()
+        
+        overrides.keys.forEach{
+            attributes[$0] = overrides[$0]
+        }
+        
+        return attributes
+    }
+    
+    private func makeAttributes() -> Dictionary<String, Any> {
+        
+        var attributes = Dictionary<String, Any>()
+        
+        // NSFontAttributeName
+        attributes[NSFontAttributeName] = defaultFont
+        
+        // NSParagraphStyleAttributeName
+        
+        // NSForegroundColorAttributeName
+        attributes[NSForegroundColorAttributeName] = defaultTextColor
+        
+        // NSBackgroundColorAttributeName
+        if let backgroundColor = defaultBackgroundColor {
+            attributes[NSBackgroundColorAttributeName] = backgroundColor
+        }
+        
+        // NSLigatureAttributeName
+        
+        // NSKernAttributeName
+        attributes[NSKernAttributeName] = defaultKerning
+        
+        // NSStrikethroughStyleAttributeName
+        attributes[NSStrikethroughStyleAttributeName] = defaultUseStrikethrough
+        
+        // NSUnderlineStyleAttributeName
+        attributes[NSUnderlineStyleAttributeName] = defaultUseUnderline
+        
+        // NSStrokeColorAttributeName
+        if let strokeColor = defaultStrokeColor {
+            attributes[NSStrokeColorAttributeName] = strokeColor
+        }
+        
+        // NSStrokeWidthAttributeName
+        attributes[NSStrokeWidthAttributeName] = defaultStokeWidth
+        
+        // NSShadowAttributeName
+        
+        // NSTextEffectAttributeName
+        
+        // NSAttachmentAttributeName
+        
+        // NSLinkAttributeName
+        
+        // NSBaselineOffsetAttributeName
+        
+        // NSUnderlineColorAttributeName
+        
+        // NSStrikethroughColorAttributeName
+        
+        // NSObliquenessAttributeName
+        
+        // NSExpansionAttributeName
+        
+        // NSWritingDirectionAttributeName
+        
+        // NSVerticalGlyphFormAttributeName
+        
+        return attributes
+    }
+    
+    
     
     
     
--- Example/Example.playground/Contents.swift
+++ Example/Example.playground/Contents.swift
@@ -6,3 +6,16 @@ import AttributedStringBuilder
 var str = "Hello, playground"
 
 let builder = AttributedStringBuilder()
+builder.defaultFont = UIFont.systemFont(ofSize: 16, weight: UIFontWeightSemibold)
+builder.defaultTextColor = UIColor.orange
+builder.defaultKerning = 1
+//builder.defaultBackgroundColor = UIColor.red
+builder.defaultUseUnderline = true
+//builder.defaultStrokeColor = UIColor.magenta
+//builder.defaultStokeWidth = 1.0
+
+
+builder.appendText("Hello")
+builder.appendText("This is a test")
+
+builder.attributedString
